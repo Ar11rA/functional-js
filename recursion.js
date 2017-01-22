11f728a9b5b09b0ad617fb656482fcae91a5c3d0
Ar11rA <aritra.ghosh2295@yahoo.com>
2017-01-22 19:07:31 +0530

    Added lesson 7 - Recursion

--- recursion.js
+++ recursion.js
@@ -1,3 +1,11 @@
- function reduce(arr, fn, initial) {
-     
+var index = 0;
+function reduce(arr, fn, initial) {
+if(index === arr.length){
+    return initial;
+}
+fn(initial,arr[index],index,arr);
+index++;
+reduce(arr, fn, initial);
+return initial;
       }
+module.exports = reduce
